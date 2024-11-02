0835b11728f5b83a87601101d0e9ba2f920c94ca
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-11-01 19:38:50 -0700

    Update constants.ts
    Idk

--- docs/.vitepress/constants.ts
+++ docs/.vitepress/constants.ts
@@ -1,6 +1,6 @@
 export const meta = {
     name: 'Site4',
     description: 'A wide array of guides and resources',
-    hostname: 'https://ld3z.github.io/site4',
+    hostname: 'https://ld3z.github.io/site4/',
     keywords: ['steam', 'roblox', 'gog', 'game-guide', 'cuphead']
   }
