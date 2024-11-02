32f594a81d8c96e3a3878dd928878124870f6325
ld3z <ldeziangamer@gmail.com>
2024-11-01 17:15:41 -0700

    update site to further add more posty bits

---
+++ docs/.vitepress/theme/PostLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Authors from './components/Authors.vue'
+
+const props = defineProps<{
+  authors: string
+}>()
+
+const formatDate = (raw: string): string => {
+  const date = new Date(raw)
+  return date.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric'
+  })
+}
+
+const { frontmatter } = useData()
+const authors = computed(() => props.authors.split(','))
+</script>
+
+<template>
+  <h1>{{ frontmatter.title }}</h1>
+  <div>{{ frontmatter.description }} • {{ formatDate(frontmatter.date) }}</div>
+  <Authors :authors="authors" />
+</template>
---
+++ docs/.vitepress/theme/components/Authors.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  authors: string[]
+}>()
+
+interface Author {
+  name: string
+  github: string
+}
+
+const data = [
+  {
+    name: 'ld3z',
+    github: 'https://github.com/ld3z'
+  }
+] satisfies Author[]
+
+const authors = computed(() =>
+  data.filter((author) => props.authors.includes(author.name))
+)
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-4 pt-2">
+    <div v-for="(c, index) of authors" class="flex items-center gap-2">
+      <img :src="`${c.github}.png`" class="h-8 w-8 rounded-full" />
+      <a :href="c.github">{{ c.name }}</a>
+      <span v-if="index < authors.length - 1">•</span>
+    </div>
+  </div>
+</template>
